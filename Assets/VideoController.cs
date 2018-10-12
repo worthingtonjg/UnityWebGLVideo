37c245f3f79bf187c629a138c2c540018a09b1a1
Jon Worthington <worthingtonjg@gmail.com>
2018-10-12 10:12:11 -0600

    Initial Commit

---
+++ Assets/VideoController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoController : MonoBehaviour
+{
+    public string videoFile;
+    public GameObject text;
+
+    private VideoPlayer videoPlayer;
+
+	// Use this for initialization
+	void Start () {
+        var textComponent = text.GetComponent<Text>();
+
+        string url = Path.Combine(Application.streamingAssetsPath, videoFile);
+        textComponent.text = url;
+
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.url = url;
+        videoPlayer.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
